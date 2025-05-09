5eb54860517f0a82b83bf5a25486a7fd6663f4b8
MohanBabup <151204286+MohanBabu-git@users.noreply.github.com>
2025-05-09 11:31:49 +0530

    Updated files after initial push

--- app.py
+++ app.py
@@ -216,4 +216,5 @@ def api_report_data():
 # ++++++++++++++++++++++++++++++++++++++ END OF REPORT PAGE +++++++++++++++++++++++++++++
 
 if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
     app.run(debug=True)
